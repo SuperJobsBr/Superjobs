a23a48c3e1d462e1d068e2b00e00b44acc7016f3
herenow <leonardoshiro@gmail.com>
2016-07-26 13:09:50 -0300

    Production configs for heroku deployment
    Manage all secrets via environment variables, so we don't need to
    prod.secrets.exs anymore.
    
    And force ssl connections on everyone, or redirect them to the ssl
    website.

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,17 @@ use Mix.Config
 # which you typically run after static files are built.
 config :superjobs, Superjobs.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: System.get_env("BASE_HOST") || "superjobs.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
   cache_static_manifest: "priv/static/manifest.json"
+
+# Database config
+config :superjobs, Superjobs.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  pool_size: 20
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -59,7 +68,3 @@ config :logger, level: :info
 # for the new static assets to be served after a hot upgrade:
 #
 #     config :superjobs, Superjobs.Endpoint, root: "."
-
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
